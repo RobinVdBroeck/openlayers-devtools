263b0033e81124d2c124662b218996aff650feaa
Robin Van den Broeck <robin.broeck@gmail.com>
2023-05-16 23:44:02 +0200

    feature: Add ViewInfo

--- src/Devtools.tsx
+++ src/Devtools.tsx
@@ -2,6 +2,7 @@ import { createSignal, onCleanup, For } from "solid-js";
 import type Map from "ol/Map";
 import type { ObjectEvent } from "ol/Object";
 import { LayerRow } from "./LayerRow";
+import { ViewInfo } from "./ViewInfo";
 
 interface DevtoolsProps {
   map: Map;
@@ -9,6 +10,8 @@ interface DevtoolsProps {
 
 export const Devtools = (props: DevtoolsProps) => {
   const mapLayers = props.map.getLayers();
+  const view = props.map.getView();
+
   const [count, setCount] = createSignal(mapLayers.getLength());
   const onChangeLength = (event: ObjectEvent) => {
     // TODO: test this
@@ -19,10 +22,10 @@ export const Devtools = (props: DevtoolsProps) => {
     mapLayers.un("change:length", onChangeLength);
   });
 
-  console.log(mapLayers.getArray());
   return (
     <div>
       <h1>Devtools</h1>
+      <ViewInfo view={view} />
       <h2>Layers: {count()}</h2>
       <ul>
         <For each={mapLayers.getArray()}>
---
+++ src/ViewInfo.tsx
@@ -0,0 +1,65 @@
+import { Show, from, Accessor } from "solid-js";
+import type OlView from "ol/View";
+import type { Projection } from "ol/proj";
+import type { Coordinate } from "ol/coordinate";
+import { type Property, toProperty } from "./property";
+import { PropertyTable } from "./PropertyTable";
+
+interface ViewData {
+  center: Coordinate | undefined;
+  projection: Projection;
+}
+
+interface ViewProps {
+  view: OlView;
+}
+
+/**
+ * Display information about an ol view
+ */
+export const ViewInfo = (props: ViewProps) => {
+  const data = from<ViewData>((set) => {
+    const sync = () =>
+      set({
+        center: props.view.getCenter(),
+        projection: props.view.getProjection(),
+      });
+    sync();
+    props.view.on("change", sync);
+    return () => props.view.un("change", sync);
+  }) as Accessor<ViewData>;
+
+  const viewProperties = from<Property[]>((set) => {
+    const syncProperties = () => {
+      const olProps = props.view.getProperties();
+      const properties = Object.entries(olProps).map(([key, value]) =>
+        toProperty(key, value)
+      );
+      set(properties);
+    };
+    syncProperties();
+    props.view.on("propertychange", syncProperties);
+    return () => props.view.un("propertychange", syncProperties);
+  }) as Accessor<Property[]>;
+
+  return (
+    <>
+      <h2>View</h2>
+      <div>
+        <div>Coordinate system: {data().projection.getCode()}</div>
+        <Show when={data().center}>
+          {(center) => <div>Center: ({center().join(", ")})</div>}
+        </Show>
+        <PropertyTable
+          properties={viewProperties()}
+          changeProperty={(property) => {
+            props.view.setProperties({
+              ...props.view.getProperties(),
+              [property.name]: property.value,
+            });
+          }}
+        />
+      </div>
+    </>
+  );
+};
